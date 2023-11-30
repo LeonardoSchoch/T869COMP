1282e99270bd815c370f148217885b922364ff27
LeonardoSchoch <leonardoschoch@gmail.com>
2023-11-30 08:01:46 +0000

    Exceptions added

--- assignment3/capture.py
+++ assignment3/capture.py
@@ -23,6 +23,21 @@ target_width = 720
 target_height = 480
 # --------------------------
 
+def remove_similar_lines(lines):
+    # Remove lines with a dot product too close to 1
+    indices_to_remove = set()
+    for i in range(len(lines)):
+        for j in range(i + 1, len(lines)):
+            x11, y11, x12, y12 = lines[i]
+            x21, y21, x22, y22 = lines[j]
+            v1 = np.array([x12 - x11, y12 - y11])
+            v2 = np.array([x22 - x21, y22 - y21])
+            dot_product = np.dot(v1, v2)
+            if abs(dot_product) > 0.95 * np.linalg.norm(lines[i]) * np.linalg.norm(lines[j]):
+                indices_to_remove.add(i)
+    return np.delete(lines, indices_to_remove)
+
+
 def points_2_line(points):
     x1 = np.append(points[:2], 1)
     x2 = np.append(points[2:], 1)
@@ -81,31 +96,40 @@ while True:
     edge_points_xy = edge_points_yx[:, [1, 0]]
     edge_points_xy = edge_points_xy[::k]
     
-    # detect 4 most prominent lines with Hough and print them on frame
+    # detect most prominent lines with Hough
     lines = cv2.HoughLinesP(edges, hough_rho, hough_theta, hough_threshold, None, hough_min_line_length, hough_max_line_gap).squeeze()
     
-    if lines is not None:
+    # try to draw lines
+    try:
+        # selected_lines = remove_similar_lines(lines)
         selected_lines = lines[:4]
         for line in selected_lines:
             x1, y1, x2, y2 = line
             cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 255), 2)
-        
+    except Exception as e:
+        print(f"{e}")
+       
+    # try to draw corners
+    try:
         corners = find_corners(selected_lines, frame.shape)
         for corner in corners:
             cv2.circle(frame, corner, 5, (0, 0, 255), -1)
+    except Exception as e:
+        print(f"{e}")
         
-        try:
-            ordered_corners = order_corners(corners)
-            target_rectangle = np.array([[0, 0],
-                                        [target_width, 0],
-                                        [target_width, target_height],
-                                        [0, target_height]])
-            h, status = cv2.findHomography(ordered_corners, target_rectangle)
-            rectified = cv2.warpPerspective(frame, h, (target_width, target_height))
-            
-            cv2.imshow('rectified', rectified)
-        except:
-            pass
+    # try to rectify frame
+    try:
+        ordered_corners = order_corners(corners)
+        target_rectangle = np.array([[0, 0],
+                                    [target_width, 0],
+                                    [target_width, target_height],
+                                    [0, target_height]])
+        h, status = cv2.findHomography(ordered_corners, target_rectangle)
+        rectified = cv2.warpPerspective(frame, h, (target_width, target_height))
+        
+        cv2.imshow('rectified', rectified)
+    except Exception as e:
+        print(f"{e}")
     
     # calculate FPS and print it on frame
     time_now = time.time()
